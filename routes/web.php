a6b2ef812b3e81887755d0f585f2e7b81dd90aa5
Khaled Hossain <khaledhossain778@gmail.com>
2025-02-26 08:23:43 +0600

    project initialization

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MailController;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
     return view('welcome');
-})->name('dashboard');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::group(['prefix' => 'mail', 'as' => 'mail.'], function () {
     Route::get('', [MailController::class, 'index'])->name('index');
     Route::get('create', [MailController::class, 'create'])->name('create');
     Route::post('send', [MailController::class, 'sendMail'])->name('send');
 });
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
